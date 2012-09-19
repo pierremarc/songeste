108dfe6de0a761427b1a5738ad0f6885e83a3d9c
Pierre Marchand <pierre@oep-h.com>
2012-09-19 13:54:51 +0200

    cleanup pyc files and allow for default entry point

--- mapo/views/home.py
+++ mapo/views/home.py
@@ -6,6 +6,7 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.views.decorators.csrf import csrf_exempt
 from songeste.mapo.models import * 
+from django.conf import settings
 import json
 
 
@@ -14,5 +15,9 @@ def handle(request):
     if 'c' in request.GET:
         comp = request.GET['c'].split(',')
         param['comp'] = comp
-        
+    else:
+        try:
+            param['comp'] = [settings.DEFAULT_COMP]
+        except Exception:
+            pass
     return render_to_response("home.html", param, context_instance = RequestContext(request))
